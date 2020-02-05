867078120910ebc48ca2dd46ee600ae273fd723a
Valeriia <v.kovalkovska@gmail.com>
2020-02-05 13:05:15 +0200

    instruction button

--- src/followups/FollowupInfo.js
+++ src/followups/FollowupInfo.js
@@ -6,6 +6,11 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles({
   card: {
@@ -15,11 +20,19 @@ const useStyles = makeStyles({
   title: {
     fontSize: 14,
   },
+  info: {
+    width: '200px',
+  },
+  wrap: {
+    margin: '20px',
+  }
 });
 
 export default function FollowupInfo(props) {
   const classes = useStyles();
   const {followup} = props;
+  const [isShowing, setIsShowing] = React.useState(true);
+
   return (
     <Card className={classes.card}>
       <CardContent>
@@ -46,7 +59,27 @@ export default function FollowupInfo(props) {
         </Grid>
       </CardContent>
       <CardActions>
-        <Button size="small">Show More</Button>
+        {
+        
+          isShowing ? <Button size="small" onClick={() => setIsShowing(!isShowing)}>Show More</Button>
+            : 
+              <Table className={classes.wrap}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Instruction</TableCell>
+                    <TableCell>Parameters</TableCell>
+                    <TableCell>State</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>{followup.instruction}</TableCell>
+                    <TableCell>{followup.parameters}</TableCell>
+                    <TableCell>{followup.state}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table> 
+        }
       </CardActions>
     </Card>
   );
--- src/followups/FollowupsGroup.js
+++ src/followups/FollowupsGroup.js
@@ -8,28 +8,40 @@ const FollowupsGroup = () => {
           "subject": "Fill details form",
           "target" : "Nick Bolton",
           "type": "cc",
-          "date": "24/01/2020 00:00"
+          "date": "24/01/2020 00:00",
+          "instruction": "info about type 'cc'",
+          "parameters": "parameters for details form",
+          "state": "passes"
         },
         {
           "from": "Bob Johnson",
           "subject": "Fill form",
           "target" : "Nick",
           "type": "to",
-          "date": "25/01/2020 00:00"
+          "date": "25/01/2020 00:00",
+          "instruction": "info about type 'to'",
+          "parameters": "parameters for form",
+          "state": "failed"
         },
         {
           "from": "Helen",
           "subject": "Form",
           "target" : "Ken",
           "type": "bcc",
-          "date": "26/01/2020 00:00"
+          "date": "26/01/2020 00:00",
+          "instruction": "info about type 'bcc'",
+          "parameters": "parameters for form",
+          "state": "passes"
         },
         {
           "from": "Emilia",
           "subject": "Fill details form",
           "target" : "Nick Bolton",
           "type": "replyto",
-          "date": "27/01/2020 00:00"
+          "date": "27/01/2020 00:00",
+          "instruction": "info about type 'replyto'",
+          "parameters": "parameters for details form",
+          "state": "failed"
         }
     ]
     const [data, setData] = React.useState(dataArr);
